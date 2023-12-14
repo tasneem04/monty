0a6e138c4e0b8ff78ab52ac8a39e3c4ffcdebf2c
tasneem04 <eng.wr.tasneem@gmail.com>
2023-12-14 13:27:27 -0800

    delete file

--- main.c
+++ main.c
@@ -1,10 +1,6 @@
 #include "monty.h"
 int main(int argc, char *argv[])
 {  
-    unsigned int line_number;	
-    FILE *file;
-    char *line;
-    size_t len;
     (void) argv;
     
     if(argc != 2)
@@ -12,27 +8,12 @@ int main(int argc, char *argv[])
 	    fprintf(stderr,"USAGE: monty file\n");
 	    exit(EXIT_FAILURE);
     }
-    file = fopen(argv[1], "r");
-    if (!file) {
-	    fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-	    exit(EXIT_FAILURE);
-    }
-
-    line = NULL;
-    len = 0;
-    line_number = 0;
-
-    while (getline(&line, &len, file) != -1) {
-        line_number++;
-    }
+  
     push(1);
     push(2);
     push(3);
     
     pall();
-   
-    free(line); 
-    fclose(file);
-    
+
     return 0;
 }
