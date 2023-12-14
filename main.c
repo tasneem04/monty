a6a8893ff62d5f275bdbb87c511e7961e3e44f0f
tasneem04 <eng.wr.tasneem@gmail.com>
2023-12-14 10:46:36 -0800

    Add condition to check main var

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include "monty.h"
-int main()
-{
+int main(int argc, char **argv)
+{  
+    (void) argv;
+    if(argc != 2)
+    {
+	    fprintf(stderr,"USAGE: monty file\n");
+	    exit(EXIT_FAILURE);
+    }
+
     push(1);
     push(2);
     push(3);
