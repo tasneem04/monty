b3a3d658c2c9f37b28bbcf5ca8182ddc1bf10866
tasneem04 <eng.wr.tasneem@gmail.com>
2023-12-16 07:41:59 -0800

    Add isdigit

--- main.c
+++ main.c
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
 		{
 		 if (strcmp(token, "push") != 0) {
                     integerValue = atoi(token);
-		    if(isInteger(integerValue))
+		    if(isdigit(integerValue))
 		    {
                     push(integerValue);
 		    }
